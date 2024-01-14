ab84ae281d7d5d4c41bf09debac0bc0a1396a74f
Michael <enjoyweaver@gmail.com>
2024-01-13 18:09:47 -0600

    main

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -6,6 +6,7 @@ import { Card } from "../components/card";
 import { Article } from "./article";
 import { Redis } from "@upstash/redis";
 import { Eye } from "lucide-react";
+import motivatelogo from "../art/motivatelogo.png";
 
 const redis = Redis.fromEnv();
 
@@ -13,7 +14,7 @@ export const revalidate = 60;
 export default async function ProjectsPage() {
   const views = (
     await redis.mget<number[]>(
-      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":")),
+      ...allProjects.map((p) => ["pageviews", "projects", p.slug].join(":"))
     )
   ).reduce((acc, v, i) => {
     acc[allProjects[i].slug] = v ?? 0;
@@ -29,12 +30,12 @@ export default async function ProjectsPage() {
       (project) =>
         project.slug !== featured.slug &&
         project.slug !== top2.slug &&
-        project.slug !== top3.slug,
+        project.slug !== top3.slug
     )
     .sort(
       (a, b) =>
         new Date(b.date ?? Number.POSITIVE_INFINITY).getTime() -
-        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime(),
+        new Date(a.date ?? Number.POSITIVE_INFINITY).getTime()
     );
 
   return (
@@ -43,10 +44,11 @@ export default async function ProjectsPage() {
       <div className="px-6 pt-20 mx-auto space-y-8 max-w-7xl lg:px-8 md:space-y-16 md:pt-24 lg:pt-32">
         <div className="max-w-2xl mx-auto lg:mx-0">
           <h2 className="text-3xl font-bold tracking-tight text-zinc-100 sm:text-4xl">
-            Projects
+            Our 3D Art
           </h2>
           <p className="mt-4 text-zinc-400">
-            Some of the projects are from work and some are on my own time.
+            The projects we have completed along and over time we will include
+            tutorials for some of them.
           </p>
         </div>
         <div className="w-full h-px bg-zinc-800" />
@@ -70,7 +72,7 @@ export default async function ProjectsPage() {
                   <span className="flex items-center gap-1 text-xs text-zinc-500">
                     <Eye className="w-4 h-4" />{" "}
                     {Intl.NumberFormat("en-US", { notation: "compact" }).format(
-                      views[featured.slug] ?? 0,
+                      views[featured.slug] ?? 0
                     )}
                   </span>
                 </div>
@@ -104,32 +106,40 @@ export default async function ProjectsPage() {
         <div className="hidden w-full h-px md:block bg-zinc-800" />
 
         <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 0)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 1)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
-          </div>
-          <div className="grid grid-cols-1 gap-4">
-            {sorted
-              .filter((_, i) => i % 3 === 2)
-              .map((project) => (
-                <Card key={project.slug}>
-                  <Article project={project} views={views[project.slug] ?? 0} />
-                </Card>
-              ))}
+          <div className="grid grid-cols-1 gap-4 mx-auto lg:mx-0 md:grid-cols-3">
+            <div className="grid grid-cols-1 gap-4">
+              {sorted
+                .filter((_, i) => i % 3 === 0)
+                .map((project) => (
+                  <ImageCard
+                    key={project.slug}
+                    project={project}
+                    views={views[project.slug] ?? 0}
+                  />
+                ))}
+            </div>
+            <div className="grid grid-cols-1 gap-4">
+              {sorted
+                .filter((_, i) => i % 3 === 1)
+                .map((project) => (
+                  <ImageCard
+                    key={project.slug}
+                    project={project}
+                    views={views[project.slug] ?? 0}
+                  />
+                ))}
+            </div>
+            <div className="grid grid-cols-1 gap-4">
+              {sorted
+                .filter((_, i) => i % 3 === 2)
+                .map((project) => (
+                  <ImageCard
+                    key={project.slug}
+                    project={project}
+                    views={views[project.slug] ?? 0}
+                  />
+                ))}
+            </div>
           </div>
         </div>
       </div>
